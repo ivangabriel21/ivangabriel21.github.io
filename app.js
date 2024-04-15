6b848e1fd3c4fa1c17b8aa33e09232ab5677090e
ivangabriel21 <103368415+ivangabriel21@users.noreply.github.com>
2024-04-15 11:50:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ document.querySelector('.sign-up-form').addEventListener('submit', async (e) => 
     const password = document.querySelector('.sign-up-form input[name="password"]').value;
 
     try {
-        const response = await fetch('http://vps.quickm.net:3000/api/signup', {
+        const response = await fetch('http://2.tcp.ngrok.io:12194/api/signup', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
