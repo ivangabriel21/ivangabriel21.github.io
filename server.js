41ee93741b6311597f15d2bb0de31e025d241d9b
ivangabriel21 <103368415+ivangabriel21@users.noreply.github.com>
2024-04-15 11:46:31 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,61 +1,58 @@
-const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
 
-const app = express();
-
-// Configura la conexión a MySQL
-const db = mysql.createConnection({
-  host: 'brntndxplxv1nrf0psbk-mysql.services.clever-cloud.com',
-  port: '21145',
-  user: 'uip1dihzrnz8kwwv',
-  password: 'TkqIu9DoCpQZC7CKbF7',
-  database: 'brntndxplxv1nrf0psbk'
+sign_up_btn.addEventListener('click', () =>{
+    container.classList.add("sign-up-mode");
 });
 
-// Conéctate a la base de datos MySQL
-db.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log('Conexión a la base de datos MySQL establecida');
+sign_in_btn.addEventListener('click', () =>{
+    container.classList.remove("sign-up-mode");
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+document.querySelector('.sign-up-form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const username = document.querySelector('.sign-up-form input[name="username"]').value;
+    const email = document.querySelector('.sign-up-form input[name="email"]').value;
+    const password = document.querySelector('.sign-up-form input[name="password"]').value;
 
-// Ruta para manejar la creación de usuarios
-app.post('/api/signup', (req, res) => {
-  const { username, email, password } = req.body;
-  const INSERT_USER_QUERY = `INSERT INTO users (username, email, password) VALUES (?, ?, ?)`;
-  db.query(INSERT_USER_QUERY, [username, email, password], (err, result) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Error al crear el usuario');
+    try {
+        const response = await fetch('http://2.tcp.ngrok.io:12194/api/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, email, password })
+        });
+        if (response.ok) {
+            alert('Usuario creado exitosamente');
+        } else {
+            alert('Error al crear el usuario');
+        }
+    } catch (error) {
+        console.error('Error:', error);
     }
-    console.log('Usuario creado exitosamente');
-    res.status(200).send('Usuario creado exitosamente');
-  });
 });
 
-// Ruta para manejar el inicio de sesión de usuarios
-app.post('/api/signin', (req, res) => {
-  const { username, password } = req.body;
-  const SELECT_USER_QUERY = `SELECT * FROM users WHERE username = ? AND password = ?`;
-  db.query(SELECT_USER_QUERY, [username, password], (err, result) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Error al iniciar sesión');
-    }
-    if (result.length === 0) {
-      return res.status(404).send('Usuario no encontrado');
-    }
-    console.log('Inicio de sesión exitoso');
-    res.status(200).send('Inicio de sesión exitoso');
-  });
-});
+document.querySelector('.sign-in-form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const username = document.querySelector('.sign-in-form input[name="username"]').value;
+    const password = document.querySelector('.sign-in-form input[name="password"]').value;
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor backend escuchando en el puerto ${PORT}`);
+    try {
+        const response = await fetch('http://vps.quickm.net:3000/api/signin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        });
+        if (response.ok) {
+            alert('Inicio de sesión exitoso');
+        } else {
+            alert('Error al iniciar sesión');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
 });
